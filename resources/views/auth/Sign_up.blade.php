102e46cf34c458ebc3816d505ecb6fc36afc326f
alasdiel <alex.albinda9@gmail.com>
2025-05-15 23:36:48 +0800

    feature: validation errors on register

--- resources/views/auth/Sign_up.blade.php
+++ resources/views/auth/Sign_up.blade.php
@@ -63,6 +63,15 @@
                     <div class="center-btn">
                         <button type="submit" class="signup-btn">SIGN UP</button>
                     </div>
+                    
+                    <!-- validation errors -->
+                    @if ($errors->any())
+                        <ul class="px-4 py-2 bg-red-100">
+                            @foreach ($errors->all() as $error)
+                                <li class="my-2 text-red-500">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
 
                     <!-- Login Link -->
                     <div class="login-link">
